1873de0db90a22ae57832fef865906344155084a
MathePLay <Matheus.doer@gmail.com>
2023-08-14 22:31:55 -0300

    Alterada a porta para 8080

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import autentRoute from './routes/autent.route.js'
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const app = express()
 
 app.use(cors())
